bd58ddd323509721f70128c2b083edc1383b7f1f
zili wang <ziliwang@zilis-MacBook-Pro.local>
2021-09-05 20:32:10 +0800

    buf atk range

--- script/world/lottery.gd
+++ script/world/lottery.gd
@@ -5,6 +5,8 @@ var buf_pool=[]
 func _init():
     buf_pool.append({"name":"add_atk","val":10,"desc":"ATK +10"})
     buf_pool.append({"name":"add_atk_spd","val":1,"desc":"ATK SPD +1"})
+    buf_pool.append({"name":"add_atk_range","val":50,"desc":"RAN +50"})
+    buf_pool.append({"name":"add_mov_speed","val":50,"desc":"SPEED +50"})
     buf_pool.append({"name":"add_hp","val":50,"desc":"HP +50"})
     buf_pool.append({"name":"double_atk","desc":"Atk DOUBLE"})
     buf_pool.append({"name":"double_atk_spd","desc":"ATK SPD DOUBLE"})
@@ -24,5 +26,9 @@ func apply_rand_buf(chara):
         chara.atk=chara.atk*2
     elif buf_info["name"]=="double_atk_spd":
         chara.atk_spd=chara.atk_spd*2
+    elif buf_info["name"]=="add_atk_range":
+        chara.atk_range=chara.atk_range+buf_info["val"]
+    elif buf_info["name"]=="add_mov_speed":
+        chara.speed=chara.speed+buf_info["val"]
     chara.fct_mgr.show_value(buf_info["desc"], true)
     
